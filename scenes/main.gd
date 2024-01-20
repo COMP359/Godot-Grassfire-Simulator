f5e4abde808a9585c04ae8d3f2b9cffad10a2d0a
Callum Brezden <callumbrezden@gmail.com>
2024-01-20 13:15:51 -0800

    Added Export Vars

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,22 +1,25 @@
 extends Node2D
 
+@onready var tilemap = $grid
+
 var grid = []
 var grid_width = 10
 var grid_height = 10
-var start_position_x = 0
-var start_position_y = 0
-var end_position_x = 9	
-var end_position_y = -9
 
+@export_range(0, 9) var start_position_x = 0
+@export_range(-9, 0) var start_position_y = 0
+@export_range(0, 9) var finish_position_x = 9
+@export_range(-9, 0) var finish_position_y = -9
+@export_range(0, 80, 5) var percentage_obstacles: float
 
-# MAKE THE START AND END TILE BE PLACED BASED ON THE START AND END POSITIONS ABOVE
+var start_cube = Vector2i(3,0)
+var finish_cube = Vector2i(4,0)
 
 func _ready():
 	initialize_grid()
 	initialize_start_position()
-	initialize_end_position()
-	print(grid)
-	
+	initialize_finish_position()
+
 func initialize_grid():
 	for i in grid_width:
 		grid.append([])
@@ -24,7 +27,15 @@ func initialize_grid():
 			grid[i].append(0)
 
 func initialize_start_position():
+	tilemap.set_cell(0, Vector2i(start_position_x, start_position_y), 0, start_cube)
 	grid[start_position_x][start_position_y] = -1
 
-func initialize_end_position():
-	grid[end_position_x][end_position_y] = -2
+func initialize_finish_position():
+	tilemap.set_cell(0, Vector2i(finish_position_x, finish_position_y), 0, finish_cube)
+	grid[finish_position_x][finish_position_y] = -2
+
+func _on_start_pressed():
+	create_obstacles()
+
+func create_obstacles():
+	pass

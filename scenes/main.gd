99c12f776d03b89d43385b3e98a4275be704177c
Callum Brezden <callumbrezden@gmail.com>
2024-01-21 21:35:45 -0800

    Converted Internal Grid into Isometric Cords

--- scenes/main.gd
+++ scenes/main.gd
@@ -37,11 +37,11 @@ func initialize_grid():
 
 func initialize_start_position():
 	tilemap.set_cell(0, Vector2i(start_position_x, start_position_y), 0, start_cube)
-	grid[start_position_x][start_position_y] = -1
+	grid[start_position_x][-start_position_y] = -1
 
 func initialize_finish_position():
 	tilemap.set_cell(0, Vector2i(finish_position_x, finish_position_y), 0, finish_cube)
-	grid[finish_position_x][finish_position_y] = -2
+	grid[finish_position_x][-finish_position_y] = -2
 
 func _on_start_pressed():
 	clear_grid()
@@ -56,14 +56,15 @@ func create_obstacles():
 		var randomX = randi_range(0, 9)
 		var randomY = randi_range(-9, 0)
 
-		if grid[randomX][randomY] == 0:
-			grid[randomX][randomY] = -3
+		if grid[randomX][-randomY] == 0:
+			grid[randomX][-randomY] = -3
 			tilemap.set_cell(0, Vector2i(randomX, randomY), 0, obstacle_cube)
 			var timeout = max(0.05, 0.15 - (0.10 * obstaclesPlaced / numberOfObstacles))  # Decrease timeout as more obstacles are placed
 			await get_tree().create_timer(timeout).timeout
 			obstacle_sound.play()
 			obstaclesPlaced += 1
 			label.text = "Obstacles Placed: " + str(obstaclesPlaced) + "/" + str(numberOfObstacles)
+	
 
 func clear_grid():
 	for i in grid_width:
@@ -74,13 +75,13 @@ func clear_grid():
 		for j in range(-9, 1):
 			tilemap.set_cell(0, Vector2i(i, j), 0, empty_cube)
 
-	grid[start_position_x][start_position_y] = -1
-	grid[finish_position_x][finish_position_y] = -2
+	grid[start_position_x][-start_position_y] = -1
+	grid[finish_position_x][-finish_position_y] = -2
 	tilemap.set_cell(0, Vector2i(start_position_x, start_position_y), 0, start_cube)
 	tilemap.set_cell(0, Vector2i(finish_position_x, finish_position_y), 0, finish_cube)
 
 func is_valid(x, y):
-	return 0 <= x and x <= 9 and -9 <= y and y <= 0 and grid[x][y] == 0
+	return 0 <= x and x <= 9 and -9 <= y and y <= 0 and grid[x][-y] == 0
 
 func grassfire_search_algo():
 	var numberOfObstacles = int(percentage_obstacles + 0.5)
@@ -95,12 +96,16 @@ func grassfire_search_algo():
 
 	while grassfire_search.size() > 0:
 		var current_node = grassfire_search.pop_front()
-		var next_current_value = grid[current_node.x][current_node.y] + 1
+		var next_current_value = 0
+		if (current_node.x == start_position_x and current_node.y == start_position_y):
+			next_current_value = 1
+		else:
+			next_current_value = grid[current_node.x][-current_node.y] + 1
 
 		for direction in directions:
 			var new_node = current_node + direction
 			if is_valid(new_node.x, new_node.y):
-				grid[new_node.x][new_node.y] = next_current_value
+				grid[new_node.x][-new_node.y] = next_current_value
 				grassfire_search.push_back(new_node)
 				tilemap.set_cell(0, new_node, 0, search_cube)
 				add_to_set(new_node)
@@ -108,6 +113,8 @@ func grassfire_search_algo():
 		await get_tree().create_timer(0.04).timeout
 		search_sound.play()
 		label.text = "Nodes Processed: " + str(searched_nodes.size()) + "/" + str(totalNodes)
+	print(grid)
+	
 
 func add_to_set(element):
 	if element not in searched_nodes:
